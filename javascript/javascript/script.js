f7cbd0afe1e57d9baf83c7e8701ebcfacefecae7
Eider <Eider@nmsu.edu>
2026-05-05 19:44:45 -0600

    Complete Project 4 with JS and Responsive Design

---
+++ javascript/javascript/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const myForm = document.getElementById('userForm');
+    const display = document.getElementById('greetingDisplay');
+
+    myForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const name = document.getElementById('nameInput').value;
+        
+        display.innerHTML = `<h3>Hello, ${name}!</h3><p>Welcome to my Project 4 interactive page.</p>`;
+        display.style.color = "darkblue";
+    });
+});
